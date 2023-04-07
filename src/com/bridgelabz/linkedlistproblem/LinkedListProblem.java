46fbc6ed4275fd3fbb2f862d657d65d7324f0b68
shephali09 <hiremathshephali9@gmail.com>
2023-04-08 01:58:42 +0530

    Add: Delete last element from linkedlist

--- src/com/bridgelabz/linkedlistproblem/LinkedListProblem.java
+++ src/com/bridgelabz/linkedlistproblem/LinkedListProblem.java
@@ -4,25 +4,25 @@ import java.util.LinkedList;
 
 public class LinkedListProblem {
 
-	void pop(LinkedList<Integer> linkedlist) {
-		linkedlist.pop();
+	void popLast(LinkedList<Integer> linkedlist) {
+		linkedlist.pollLast();
 	}
 
 	public static void main(String[] args) {
 
-		LinkedListProblem list = new LinkedListProblem();
+		LinkedListProblem linkedlistproblem = new LinkedListProblem();
 
 		LinkedList<Integer> linkedlist = new LinkedList<Integer>();
 
 		linkedlist.add(56);
+		linkedlist.add(30);
 		linkedlist.add(70);
-		linkedlist.add(1, 30);
 
 		System.out.println(linkedlist);
 
-		list.pop(linkedlist);
+		linkedlistproblem.popLast(linkedlist);
 
-		System.out.println("After removing first element:");
+		System.out.println("After deleting last element:");
 		System.out.println(linkedlist);
 
 	}
